f9280b73834a095da19b5e63aba16f2fa07ca64b
Adi Singh <adisingh@Adis-MacBook-Pro.local>
2025-02-03 17:07:16 -0500

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :weather_forecasts, only: [ :index, :show, :new, :create ]
+  resources :weather_forecasts, only: [ :index, :create ]
   root to: "weather_forecasts#index"
 
 
